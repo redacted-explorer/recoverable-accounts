b224def1a36977c39e50e6d5f69c79b200bd0737
Slime <slime@intear.tech>
2024-11-07 01:13:21 +0200

    Verify signatures

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,34 +1,44 @@
 use near_sdk::{
-    env, near, require, AccountId, Allowance, Gas, GasWeight, NearToken, PanicOnDefault, Promise,
-    PublicKey,
+    bs58, env, near, require, AccountId, Allowance, CurveType, Gas, GasWeight, NearToken,
+    PanicOnDefault, Promise, PublicKey,
 };
 
 #[near(contract_state)]
 #[derive(PanicOnDefault)]
 pub struct Contract {
-    recovery_key: Vec<u8>,
-    user_key: Vec<u8>,
+    recovery_key: [u8; 32],
+    user_key: [u8; 32],
 }
 
 #[near]
 impl Contract {
     #[init]
-    pub fn new(recovery_key: Vec<u8>, public_key: PublicKey, signature: Vec<u8>) -> Self {
-        // TODO: check signature
+    pub fn new(recovery_key: [u8; 32], public_key: [u8; 32], signature: Vec<u8>) -> Self {
+        let Ok(signature): Result<[u8; 64], _> = signature.try_into() else {
+            env::panic_str("Invalid signature")
+        };
+        require!(
+            verify_signature(&recovery_key, format!("I want to log in to [REDACTED] Explorer and add key ed25519:{} for quick trading without confirmation in wallet", bs58::encode(public_key).into_string()).as_bytes(), &signature),
+            "Invalid signature"
+        );
         Promise::new(env::current_account_id()).add_access_key_allowance(
-            public_key.clone(),
+            if let Ok(key) = PublicKey::from_parts(CurveType::ED25519, public_key.to_vec()) {
+                key
+            } else {
+                env::panic_str("Invalid public key, only ED25519 is supported")
+            },
             Allowance::Unlimited,
             env::current_account_id(),
             "relay_transactions".to_string(),
         );
         Self {
             recovery_key,
-            user_key: public_key.into_bytes(),
+            user_key: public_key,
         }
     }
 
     pub fn get_recovery_key(&self) -> Vec<u8> {
-        self.recovery_key.clone()
+        self.recovery_key.to_vec()
     }
 
     #[private]
@@ -53,15 +63,36 @@ impl Contract {
         promise
     }
 
-    pub fn recover(&mut self, public_key: PublicKey, recovery_signature: Vec<u8>) -> Promise {
-        // TODO: check signature
-        let old_key = self.user_key.clone().try_into().unwrap();
-        self.user_key = public_key.clone().into_bytes();
+    pub fn recover(&mut self, new_public_key: [u8; 32], recovery_signature: Vec<u8>) -> Promise {
+        let Ok(recovery_signature): Result<[u8; 64], _> = recovery_signature.try_into() else {
+            env::panic_str("Invalid signature")
+        };
+        require!(
+            verify_signature(
+                &self.recovery_key,
+                format!("I want to log in to [REDACTED] Explorer and add key ed25519:{} for quick trading without confirmation in wallet", bs58::encode(new_public_key).into_string()).as_bytes(),
+                &recovery_signature
+            ),
+            "Invalid signature"
+        );
+        self.user_key = new_public_key;
         Promise::new(env::current_account_id())
-            .delete_key(old_key)
+            .delete_key(
+                if let Ok(key) = PublicKey::from_parts(CurveType::ED25519, self.user_key.to_vec()) {
+                    key
+                } else {
+                    env::panic_str("Invalid public key, only ED25519 is supported")
+                },
+            )
             .then(
                 Promise::new(env::current_account_id()).add_access_key_allowance(
-                    public_key,
+                    if let Ok(key) =
+                        PublicKey::from_parts(CurveType::ED25519, new_public_key.to_vec())
+                    {
+                        key
+                    } else {
+                        env::panic_str("Invalid public key, only ED25519 is supported")
+                    },
                     Allowance::Unlimited,
                     env::current_account_id(),
                     "relay_transactions".to_string(),
@@ -70,10 +101,56 @@ impl Contract {
     }
 }
 
+fn verify_signature(
+    public_key: &[u8; 32],
+    message: impl Into<Vec<u8>>,
+    signature: &[u8; 64],
+) -> bool {
+    let message = message.into();
+    env::ed25519_verify(signature, &message, public_key)
+}
+
 #[near(serializers=[json])]
 pub struct RelayedTransaction {
     pub receiver_id: AccountId,
     pub method: String,
     pub args: Vec<u8>,
     pub deposit: NearToken,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_verify_signature_positive() {
+        let public_key = [
+            75, 188, 143, 161, 187, 53, 78, 9, 209, 243, 173, 201, 92, 239, 81, 65, 68, 163, 106,
+            64, 5, 95, 41, 254, 248, 55, 110, 52, 236, 34, 158, 165,
+        ];
+        let message = "I want to log in to [REDACTED] Explorer and add key ed25519:Ae3nRD5NNd7EGJqs3PmCCX5EsAycUGJxk3x9LoHpVtXu for quick trading without confirmation in wallet".as_bytes();
+        let signature = [
+            145, 195, 102, 41, 132, 32, 30, 85, 11, 218, 120, 160, 178, 180, 8, 157, 107, 132, 238,
+            152, 63, 63, 115, 181, 111, 87, 140, 244, 200, 193, 2, 58, 92, 102, 58, 197, 35, 230,
+            0, 61, 165, 174, 210, 165, 103, 233, 173, 81, 81, 86, 93, 26, 161, 225, 211, 143, 95,
+            3, 171, 173, 81, 18, 39, 7,
+        ];
+        assert!(verify_signature(&public_key, message, &signature));
+    }
+
+    #[test]
+    fn test_verify_signature_negative() {
+        let public_key = [
+            75, 188, 143, 161, 187, 53, 78, 9, 209, 243, 173, 201, 92, 239, 81, 65, 68, 163, 106,
+            64, 5, 95, 41, 254, 248, 55, 110, 52, 236, 34, 158, 165,
+        ];
+        let message = "I want to log in to [REDACTED] Explorer and add key ed25519:Ae3nRD5NNd7EGJqs3PmCCX5EsAycUGJxk3x9LoHpVtXu for quick trading without confirmation in wallet".as_bytes();
+        let signature = [
+            145, 195, 102, 41, 132, 32, 30, 85, 11, 218, 120, 160, 178, 180, 8, 157, 107, 132, 238,
+            152, 63, 63, 115, 181, 111, 87, 140, 244, 200, 193, 2, 58, 92, 102, 58, 197, 35, 230,
+            0, 61, 165, 174, 210, 165, 103, 233, 173, 81, 81, 86, 93, 26, 161, 225, 211, 143, 95,
+            3, 171, 173, 81, 18, 39, 8, // changed 7 to 8
+        ];
+        assert!(!verify_signature(&public_key, message, &signature));
+    }
 }
--- contract/tests/test.rs
+++ contract/tests/test.rs
@@ -1,4 +1,24 @@
+use near_crypto::{KeyType, SecretKey, Signature};
+
 #[tokio::test]
-async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_create_wallet() -> Result<(), Box<dyn std::error::Error>> {
+    let contract_wasm = near_workspaces::compile_project("./").await?;
+    let sandbox = near_workspaces::sandbox().await?;
+    let contract = sandbox.dev_deploy(&contract_wasm).await?;
+
+    let recovery_key = SecretKey::from_random(KeyType::ED25519);
+    let account_key = SecretKey::from_random(KeyType::ED25519);
+    let signature = recovery_key.sign(format!("I want to log in to [REDACTED] Explorer and add key {} for quick trading without confirmation in wallet", account_key.public_key().to_string()).as_bytes());
+    let outcome = contract
+        .call("new")
+        .args_json(serde_json::json!({
+            "recovery_key": recovery_key.public_key().key_data(),
+            "public_key": account_key.public_key().key_data(),
+            "signature": if let Signature::ED25519(signature) = signature { signature.to_bytes().to_vec() } else { unreachable!() },
+        }))
+        .transact()
+        .await?;
+    println!("{outcome:?}");
+    assert!(outcome.is_success());
     Ok(())
 }
